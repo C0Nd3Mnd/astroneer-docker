3b902fab283dc6733a7e55b11549b3e4122d6b57
birdhimself <git@wuul.eu>
2025-06-21 15:54:10 +0200

    Allow encryption by default

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,5 @@ cd /astrotux
 source ./venv/bin/activate
 
 python3 AstroTuxLauncher.py install
+sed -i 's/DisableEncryption = true/DisableEncryption = false/' launcher.toml
 python3 AstroTuxLauncher.py start
